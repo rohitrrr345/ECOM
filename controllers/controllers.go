77c1f2ab0b09c830e9e620c1a0b8579ff24ed9b5
rohitrrr345 <rrajput2060@gmail.com>
2024-11-11 03:12:05 +0530

    Added signup functionality

---
+++ controllers/controllers.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rohitrrr345/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+  
+
+
+
+func hashPassword(password string) string {
+
+}
+func VerifyPassword(userpassword string ,givenpassword string) (bool,string) {
+
+}
+    
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context){
+		var ctx,cancel=context.WithTimeout((context.Background(),100*time.Second))
+		defer cancel()
+		var user models.User
+		if err:=c.BindJSON(&user);err!=nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+          return
+		}
+		validationErr:=Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+		count,err:=UserCollection.CountDocuments(ctx,bson.M{"email",user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count>0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+
+		}
+		count,err=UserCollection.CountDocuments(ctx,bson.M{"phone",user.Phone})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone is already in use"})
+			return
+		}
+
+		password:=hashPassword(*user.Password)
+		user.Password=&password
+		user.Created_At,_=time.Parse(time.RFC339,time.Now().Format(time.RFC3339))
+		user.Updated_At,_=time.Parse(time.RFC3339,time.Now().Format(time.RFC3339))
+		user.ID=primitive.NewObjectID()
+		user.User_ID=user.ID.Hex()
+		token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
+		user.Token=&token
+		user.Refresh_Token=&refreshtoken
+		user.UserCart=make([]models.ProductUser,0)
+		user.Address_Details=make([]models.Address,0)
+		user.Order_Status=make([]models.Order,0)
+		_,insertErr:=UserCollection.InsertOne(ctx,user)
+		if insertErr!=nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "not created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusCreated, "Successfully Signed Up!!")
+        
+
+	}
+}
---
+++ database/cart.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+	"github.com/rohitrrr345/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+var (
+	ErrCantFindProduct    = errors.New("can't find product")
+	ErrCantDecodeProducts = errors.New("can't find product")
+	ErrUserIDIsNotValid   = errors.New("user is not valid")
+	ErrCantUpdateUser     = errors.New("cannot add product to cart")
+	ErrCantRemoveItem     = errors.New("cannot remove item from cart")
+	ErrCantGetItem        = errors.New("cannot get item from cart ")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
+)
+
